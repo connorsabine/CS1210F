26a3d3cbf17d4745e40613e5b0948f3df08f21dc
connorsabine <connorsabine@icloud.com>
2024-09-04 14:17:54 -0400

    Updated day_three.py

--- notes/day_three.py
+++ notes/day_three.py
@@ -2,12 +2,44 @@ from formatting import intro, line_break, line_with_note
 
 intro(2)
 
-print("how would i determine if a number is odd or even?")
+# Mod
+# how would i determine if a number is odd or even?
+# Use Modulus to find if their is a remainder when deviding by two
+# if there is a remainder (1), the value of num is odd
+# if there is no remainder (0), the value of num is even
 
-num = 1234568798564
-print("Odd") if num % 2 != 0 else print("Even")
+# Example usage of mod
+num = 5
+print(num % 2) # Output: 1 (Odd)
 
+num = 6
+print(num % 2) # Output: 0 (Even)
 
+line_break()
+
+# Functions
+# Functions are reusable blocks of code that perform a specific task.
+# A function is defined using the 'def' keyword followed by the function name and parentheses.
+# Inside the parentheses, you can specify any parameters that the function needs.
+
+# Example: Cube function
+# The cube function takes a number as input and returns its cube.
+def cube(x):
+    """
+    Parameters:
+    x (int or float): The number to be cubed.
+    
+    Returns:
+    int or float: The cube of the input number.
+    """
+    return x**3
+
+# Example usage of the cube function
+print(cube(3))  # Output: 27
+
+line_break()
+
+# Functions Made in Class
 def cube(x):
     return x**3
 
